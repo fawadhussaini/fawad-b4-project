6f06ed2027dc5a709a6b092117293da75ec6a657
Fawad Hussaini <fawadhussaini@Fawads-MacBook-Pro.local>
2021-08-29 09:39:47 -0400

    this is the second day of Git

---
+++ src/company1/company/GitDay2Fawad.java
@@ -0,0 +1,7 @@
+package company1.company;
+
+public class GitDay2Fawad {
+    public static void main(String[] args) {
+        System.out.println("the second day of Git.");
+    }
+}
---
+++ src/company1/company/Main.java
@@ -0,0 +1,9 @@
+package company1.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is fawad's first project");
+    }
+}
